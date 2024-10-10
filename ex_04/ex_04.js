25fa3326fa819fb3b6035d96f94a4095f8ec3950
Abdallah94212 <abdallah.borji@epitech.digital>
2024-10-10 15:11:12 +0200

    Delete ex_04 directory

--- ex_04/ex_04.js
+++
@@ -1,9 +0,0 @@
-// La fonction houdini pour cacher tous les paragraphes
-function houdini() {
-   $('p').hide();  // Sélectionne tous les éléments <p> et les cache
-}
-
-// Quand le bouton est cliqué, la fonction houdini est appelée
-$('#hide-btn').click(function() {
-   houdini();
-});
